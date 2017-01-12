1c3767cb245941cc063443ac8d467a5a32dac035
MinuraIddamalgoda <mini2476@gmail.com>
2017-01-12 15:20:08 +1100

    Finished of first app

--- 1 - ScrollView With Buttons/1 - ScrollView With Buttons/ViewController.swift
+++ 1 - ScrollView With Buttons/1 - ScrollView With Buttons/ViewController.swift
@@ -13,25 +13,43 @@ class ViewController: UIViewController {
     // MARK: - Variables
     var images = [UIImageView]()
 
-    // MARK: - Outlets 
+    // MARK: - Outlets
     @IBOutlet weak var scrollView: UIScrollView!
     
+    @IBOutlet var rightTapRecognizer: UITapGestureRecognizer!
+    @IBOutlet var leftTapRecognizer: UITapGestureRecognizer!
     
+    @IBOutlet weak var rightArrowImage: UIImageView!
+    @IBOutlet weak var leftArrowImage: UIImageView!
+    
+    // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        // Setting up tap recognisers
+        let rightTap = UITapGestureRecognizer.init(target: self, action: #selector(self.respondToRightTap))
+        rightArrowImage.addGestureRecognizer(rightTap)
+        rightArrowImage.isUserInteractionEnabled = true
+        
+        let leftTap = UITapGestureRecognizer.init(target: self, action: #selector(self.respondToLeftTap))
+        leftArrowImage.addGestureRecognizer(leftTap)
+        leftArrowImage.isUserInteractionEnabled = true
     }
     
     // Can't get size of views in viewDidLoad(), only after they appear on
     // screen can you get their size
     override func viewDidAppear(_ animated: Bool) {
         
-        var contentWidth: CGFloat = 0.0
+        // The dimensions of the scrollView
         let scrollWidth = scrollView.frame.size.width
         let scrollHeight = scrollView.frame.size.height
+        // The width of the individual scrolling panels
+        var contentWidth: CGFloat = 0.0
         
+        // Three loops for three images
         for x in 0...2 {
+            // Getting the image resource and casting it 
+            // as a UIImageView
             let image = UIImage(named: "image\(x).png")
             let imageView = UIImageView(image: image)
             images.append(imageView)
@@ -45,13 +63,31 @@ class ViewController: UIViewController {
         
         contentWidth = scrollWidth * CGFloat(images.count)
         
-        scrollView.clipsToBounds = true
+        scrollView.clipsToBounds = false
         
-        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        scrollView.contentSize = CGSize(width: contentWidth, height: 1.0)
         
         print("Count:  \(images.count)")
     }
 
+    // MARK: Tap Gesture Methods
+    func respondToRightTap() {
+        print("right tapped")
+        let contentOffsetX = scrollView.contentOffset.x
+        let contentOffsetY = scrollView.contentOffset.y
+        let scrollWidth = scrollView.frame.width
+
+        scrollView.setContentOffset(CGPoint(x: contentOffsetX + scrollWidth, y: contentOffsetY), animated: true)
+    }
+    
+    func respondToLeftTap() {
+        print("left tapped")
+        let contentOffsetX = scrollView.contentOffset.x
+        let contentOffsetY = scrollView.contentOffset.y
+        let scrollWidth = scrollView.frame.width
+        
+        scrollView.setContentOffset(CGPoint(x: contentOffsetX - scrollWidth, y: contentOffsetY), animated: true)
+    }
 
 }
 
