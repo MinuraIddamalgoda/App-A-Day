c90f1d4c1ec34c3b40263aa87f1388be0afc4218
MinuraIddamalgoda <mini2476@gmail.com>
2017-01-26 19:17:20 +1100

    last commit

---
+++ 8 - MovieDB/8 - MovieDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  8 - MovieDB
+//
+//  Created by Minura Iddamalgoda on 26/1/17.
+//  Copyright © 2017 Minura Iddamalgoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
