440a5fefd671b6342de92f459a98eaf6083ee897
MinuraIddamalgoda <mini2476@gmail.com>
2017-02-09 23:47:24 +1100

    finished front end and implemented tap recognisers

---
+++ 10 - Colour Me/10 - Colour Me/TintImage.swift
@@ -0,0 +1,19 @@
+//
+//  TintImage.swift
+//  10 - Colour Me
+//
+//  Created by Minura Iddamalgoda on 9/2/17.
+//  Copyright © 2017 Minura Iddamalgoda. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TintImage: UIImage {
+    
+    
+    func tintImage(image: UIImage) -> UIImage {
+        
+        return image
+    }
+}
--- 10 - Colour Me/10 - Colour Me/ViewController.swift
+++ 10 - Colour Me/10 - Colour Me/ViewController.swift
@@ -9,17 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    // MARK: Tap Gesture Recognisers
+    @IBOutlet var greenTapRecog: UITapGestureRecognizer!
+    @IBOutlet var redTapRecog: UITapGestureRecognizer!
+    @IBOutlet var purpleTapRecog: UITapGestureRecognizer!
+    @IBOutlet var blackTapRecog: UITapGestureRecognizer!
+    @IBOutlet var pinkTapRecog: UITapGestureRecognizer!
+    @IBOutlet var blueTapRecog: UITapGestureRecognizer!
+    
+    // MARK: Stack Views
+    @IBOutlet weak var greenStackView: UIStackView!
+    @IBOutlet weak var redStackView: UIStackView!
+    @IBOutlet weak var purpleStackView: UIStackView!
+    @IBOutlet weak var blackStackView: UIStackView!
+    @IBOutlet weak var pinkStackView: UIStackView!
+    @IBOutlet weak var blueStackView: UIStackView!
+    
 
+    // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set up gesture recognisers
+        let greenTap = UITapGestureRecognizer.init(target: self, action: #selector(self.greenStackTapped))
+        let redTap = UITapGestureRecognizer.init(target: self, action: #selector(self.redStackTapped))
+        let purpleTap = UITapGestureRecognizer.init(target: self, action: #selector(self.purpleStackTapped))
+        let blackTap = UITapGestureRecognizer.init(target: self, action: #selector(self.blackStackTapped))
+        let pinkTap = UITapGestureRecognizer.init(target: self, action: #selector(self.pinkStackTapped))
+        let blueTap = UITapGestureRecognizer.init(target: self, action: #selector(self.blueStackTapped))
+        
+        // Connect gesture recognisers to stack views
+        greenStackView.addGestureRecognizer(greenTap)
+        
+        redStackView.addGestureRecognizer(redTap)
+        
+        purpleStackView.addGestureRecognizer(purpleTap)
+        
+        blackStackView.addGestureRecognizer(blackTap)
+        
+        pinkStackView.addGestureRecognizer(pinkTap)
+        
+        blueStackView.addGestureRecognizer(blueTap)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func greenStackTapped() {
+        print("Green Tapped")
     }
-
+    
+    func redStackTapped() {
+        print("Red Tapped")
+    }
+    
+    func purpleStackTapped() {
+        print("Purple Tapped")
+    }
+    
+    func blackStackTapped() {
+        print("Black Tapped")
+    }
+    
+    func pinkStackTapped() {
+        print("Pink Tapped")
+    }
+    
+    func blueStackTapped() {
+        print("Blue Tapped")
+    }
 
 }
 
