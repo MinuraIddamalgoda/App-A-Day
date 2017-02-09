4d5a86452b3c1bbd6a5093d7aff8c9ef0a757054
MinuraIddamalgoda <mini2476@gmail.com>
2017-02-09 15:08:47 +1100

    finished day nine

--- 9-Anim8/app/src/main/java/com/iddamal/min/a9_anim8/MainActivity.java
+++ 9-Anim8/app/src/main/java/com/iddamal/min/a9_anim8/MainActivity.java
@@ -8,7 +8,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mButton;
+    // Views
+    private Button mFadeInButton;
+    private Button mFadeOutButton;
+    private Button mSlideUpButton;
+    private Button mSlideDownButton;
     private TextView mTextView;
 
     @Override
@@ -16,15 +20,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mButton = (Button) findViewById(R.id.button);
+        // Setting up views
+        mFadeInButton = (Button) findViewById(R.id.btnFadeIn);
+        mFadeOutButton = (Button) findViewById(R.id.btnFadeOut);
+        mSlideUpButton = (Button) findViewById(R.id.btnUp);
+        mSlideDownButton = (Button) findViewById(R.id.btnDown);
         mTextView = (TextView) findViewById(R.id.textview);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
+        // Constants
+        final float TEXT_VIEW_X_CO = mTextView.getX();
+        final float TEXT_VIEW_Y_CO = mTextView.getY();
+
+        mFadeInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextView.animate();
+                mTextView.animate()
+                        .alpha(1.0f)
+                        .setDuration(150L);
                 
             }
         });
+
+        mFadeOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.animate()
+                        .alpha(0.0f)
+                        .setDuration(150L);
+            }
+        });
+
+        mSlideUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.setAlpha(0f);
+                //mTextView.setX(TEXT_VIEW_X_CO);
+                mTextView.setY(TEXT_VIEW_Y_CO);
+                mTextView.animate()
+                        .alpha(1f)
+                        .translationYBy(10f)
+                        .setDuration(300L);
+            }
+        });
+
+        mSlideDownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.setAlpha(0f);
+                //mTextView.setX(TEXT_VIEW_X_CO);
+                mTextView.setY(TEXT_VIEW_Y_CO);
+                mTextView.animate()
+                        .alpha(1f)
+                        .translationYBy(-10f)
+                        .setDuration(300L);
+            }
+        });
+
     }
 }
